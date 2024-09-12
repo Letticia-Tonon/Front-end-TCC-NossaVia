547b93675c3066727ea111e374e3da1dcbec8682
frsouzaa <felipe.souza1506@outlook.com>
2024-09-12 20:08:38 -0300

    fix: ajusta de bug no actionsheet

--- src/app/components/CActionSheet.js
+++ src/app/components/CActionSheet.js
@@ -18,30 +18,33 @@ export default function CActionSheet(props) {
         cancelButtonIndex,
       },
       (selectedIndex) => {
-        props.setState(props.itens[selectedIndex]);
+        if (selectedIndex !== cancelButtonIndex)
+          props.setState(props.itens[selectedIndex]);
       }
     );
   };
   return (
     <View style={{ width: "100%", margin: 8 }}>
-    <Pressable
-      style={
-        props.error ? { ...styles.button, borderColor: "#ff0022" } : styles.button
-      }
-      onPress={onPress}
-    >
-      <Text style={styles.text}>
-        {props.state ? props.state : props.placeholder}
-      </Text>
-      <View style={styles.icon}>
-        <FontAwesomeIcon
-          icon={faCaretDown}
-          size={INPUT_HEIGHT - 25}
-          color="#FF7C33"
-        ></FontAwesomeIcon>
-      </View>
-    </Pressable>
-    {props.error && props.errorMessage && (
+      <Pressable
+        style={
+          props.error
+            ? { ...styles.button, borderColor: "#ff0022" }
+            : styles.button
+        }
+        onPress={onPress}
+      >
+        <Text style={styles.text}>
+          {props.state ? props.state : props.placeholder}
+        </Text>
+        <View style={styles.icon}>
+          <FontAwesomeIcon
+            icon={faCaretDown}
+            size={INPUT_HEIGHT - 25}
+            color="#FF7C33"
+          ></FontAwesomeIcon>
+        </View>
+      </Pressable>
+      {props.error && props.errorMessage && (
         <Text style={{ color: "#ff0022" }}>{props.errorMessage}</Text>
       )}
     </View>
